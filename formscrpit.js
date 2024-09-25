ac9976b6710c2e734670b3d899e7361acaff046a
Sudharson J <J.sudharson@hotmail.com>
2024-09-25 06:56:20 +0530

    form validation

--- formscrpit.js
+++ formscrpit.js
@@ -29,3 +29,38 @@ image.addEventListener("mouseout",()=>{
     back1.classList.remove("animate");
     back2.classList.remove("animate");
 })
+
+function validation(){
+    const Name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    const nameerror = document.querySelector(".nameerror");
+    const emailerror = document.querySelector(".emailerror");
+    const messageerror =document.querySelector(".messageerror");
+    var isValid = true; 
+
+    if(Name.trim() === ""){
+        nameerror.textContent = "Invalid Name";
+        isValid=false;
+    }else{
+        nameerror.textContent=""
+    }
+    const Emailpattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if(!Emailpattern.test(email)){
+        emailerror.textContent="Enter valid E-mail";
+        isValid=false;
+    }else{
+        emailerror.textContent="";
+    }
+    if(message.trim()===""){
+        messageerror.textContent="Please leave a message";
+        isValid=false
+    }else{
+        messageerror.textContent="";
+    }
+    if(isValid){
+        alert("Successfully Submitted")
+    }else{
+        alert("Fill all fileds ")
+    }
+}
--- script.js
+++ script.js
@@ -10,3 +10,38 @@ icon.onclick = function(){
         icon.src = "moon.png";
     }
 }
+
+function validation(){
+    const Name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    const nameerror = document.querySelector(".nameerror");
+    const emailerror = document.querySelector(".emailerror");
+    const messageerror =document.querySelector(".messageerror");
+    var isValid = true; 
+
+    if(Name.trim() === ""){
+        nameerror.textContent = "Invalid Name";
+        isValid=false;
+    }else{
+        nameerror.textContent=""
+    }
+    const Emailpattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if(!Emailpattern.test(email)){
+        emailerror.textContent="Enter valid E-mail";
+        isValid=false;
+    }else{
+        emailerror.textContent="";
+    }
+    if(message.trim()===""){
+        messageerror.textContent="Please leave a message";
+        isValid=false
+    }else{
+        messageerror.textContent="";
+    }
+    if(isValid){
+        alert("Successfully Submitted")
+    }else{
+        alert("Fill all fileds ")
+    }
+}
